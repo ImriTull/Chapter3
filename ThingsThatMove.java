00e05655226d518085fb276e73e5fa97c8abcefe
Imri Tull <111070159+xretraces@users.noreply.github.com>
2022-10-03 15:11:10 -0400

    Create ThingsThatMove.java

---
+++ ThingsThatMove.java
@@ -0,0 +1,22 @@
+package Flying;
+
+public class ThingsThatMove {
+
+    public static void main(String [] args){
+
+        Airplane airplane = new Airplane("Boeing 747", 2016);
+
+        Bird bird1 = new Bird("Eagle");
+
+        Bird bird2 = new Bird("HummingBird");
+
+        Movement[] movements = {airplane, bird1, bird2};
+
+        for (Movement movement : movements){
+            System.out.println(movement);
+            movement.fly();
+            movement.walk();
+            movement.jump();
+        }
+    }
+}
